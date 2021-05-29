edc24774c701701d0dcd3f5a34e3436c0ec86170
greyguy21 <ongkaiitiing@gmail.com>
2021-05-29 18:57:44 +0800

    added firebase sdks & initialised firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:study_buddy/screens/homepage.dart';
 import 'package:study_buddy/screens/main_focus.dart';
@@ -6,17 +7,22 @@ import 'package:study_buddy/screens/authentication.dart';
 import 'package:study_buddy/screens/loading.dart';
 import 'package:study_buddy/screens/store_inventory.dart';
 
-void main() {
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'study buddy',
       theme: ThemeData(primaryColor: Colors.white),
-      home: HomePage(),
+      home: Loading(),
       initialRoute: "/",
 
       // routes: {
