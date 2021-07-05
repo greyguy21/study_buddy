641b52d24a44040bf9ab38432cd1c7c0c626721f
greyguy21 <ongkaiitiing@gmail.com>
2021-07-05 23:51:26 +0800

    imported charts and tags dependencies

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_tags/flutter_tags.dart';
 import 'package:study_buddy/models/app_user.dart';
 import 'package:study_buddy/screens/loading.dart';
 import 'package:study_buddy/services/database.dart';
@@ -15,6 +16,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
+  final GlobalKey<TagsState> _tagKey = GlobalKey<TagsState>();
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<AppUser>(
@@ -119,6 +122,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _startPopup(BuildContext context) {
+    List tags = [];
     return new AlertDialog(
       title: const Text('Focus session settings'),
       content: new Column(
@@ -275,3 +279,23 @@ class _AnimalState extends State<Animal> {
     );
   }
 }
+
+// stream of tags in database
+// user has a list of tags -> collection?
+// tag doc is name and color
+// streambuilder --> need a tag stream!
+
+// Tag Collection
+// -> build the tags in database (retrieve data)
+// -> add new tag (update data)
+// -> remove tag (update data)
+
+// User field
+// need to get current user
+// -> onPressed -> tag chosen -> update
+
+// Tasks
+// need to get current task -> another global variable??
+// need to add
+
+// figure out how to add & choose color palette
--- lib/screens/statistics.dart
+++ lib/screens/statistics.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:study_buddy/models/app_user.dart';
 import 'package:study_buddy/screens/homepage.dart';
@@ -19,10 +20,12 @@ class _StatisticsPageState extends State<StatisticsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Statistics"),
-      ),
-      // body: ,
-    );
+          appBar: AppBar(
+            title: Text("Statistics"),
+          ),
+          // body: ,
+        );
   }
 }
+
+
